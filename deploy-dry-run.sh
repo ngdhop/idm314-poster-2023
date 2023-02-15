5181114cb074f93ed70deaa5f2fe19a74325a8b8
Eric Londaits <eric.londaits@gmail.com>
2023-02-15 15:11:31 +0100

    Add deployment scripts.

---
+++ deploy-dry-run.sh
@@ -0,0 +1,1 @@
+aws s3 sync --dryrun --exclude .DS_Store --exclude .gitkeep pdf/ s3://static.idm314.org/resources/posters
---
+++ deploy.sh
@@ -0,0 +1,1 @@
+aws s3 sync --exclude .DS_Store --exclude .gitkeep pdf/ s3://static.idm314.org/resources/posters
